c82b41befbb4071edf38c81abc6cfba698fd2fc1
Khalid Oumarou Garba <khalidogarba@gmail.com>
2023-09-06 22:45:20 +0200

    refactoring btc.js

--- btc.js
+++ btc.js
@@ -18,20 +18,20 @@ async function main() {
     // Trycatch est necessaire pour gérer les potentielles erreurs d'Axios
     try { 
     
+    //L'url de l'API de Coindesk
     const url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     
     //Chemin pour accéder à l'API de Coindesk(request http) 
     const { data } = await axios.get(url);
 
     //Reponse en cas de saisi de devise inconnue
-    if (!data.data.bpi[currency]) {
+    if (!data.bpi[currency]) {
       throw new Error("Devise inconnue");
     }
-
     //Obtenir les infos concernant la dernière màj sur les crypto
-    const updatedAt = data.data.time.updated;
+    const updatedAt = data.time.updated;
     //Obtenir le taux des crypto
-    const rate = data.data.bpi[currency].rate;
+    const rate = data.bpi[currency].rate;
     //afficher le résultat
     console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
   } catch (error) {
